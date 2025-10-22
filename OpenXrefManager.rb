596c92328f42ba63098983a9f1b44ad5f23dcd32
Jure <judez.jure@gmail.com>
2025-10-22 13:44:01 +0200

    v1.2.1

---
+++ OpenXrefManager.rb
@@ -0,0 +1,31 @@
+# Open XRef Manager
+# Copyright (C) 2025 Your Name or Company Name
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+require 'sketchup.rb'
+require 'extensions.rb'
+
+# Create a new SketchupExtension object.
+open_xref_extension = SketchupExtension.new("Open XRef Manager", "OpenXrefManager/OpenXrefManager_main.rb")
+
+# Add some details that will show up in the Extension Manager.
+open_xref_extension.description = "A professional XRef management system for collaborative workflows."
+open_xref_extension.version = "1.2.1"
+open_xref_extension.creator = "Jure Judez"
+open_xref_extension.copyright = "2025, licensed under GNU GPL v3"
+
+# Register the extension with SketchUp.
+Sketchup.register_extension(open_xref_extension, true)
+
